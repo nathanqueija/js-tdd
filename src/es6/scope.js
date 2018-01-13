3f57f76e306a38e1c48e51fd0e6f9797871c7e28
Nathan Queija <nathanqueija@Nathans-MacBook-Pro.local>
2018-01-13 16:48:33 -0200

    [add] es6 files

---
+++ src/es6/scope.js
@@ -0,0 +1,25 @@
+// let variables are block scoped
+let sound = 'meow';
+sound = 'test';
+console.log(sound);
+
+// const has constant values, it is not possible to reassign new values
+// const is not an immutable object, you can not just reassign its value
+const name = 'jake';
+const nathan = {
+  name: 'Nathan',
+  age: 50,
+};
+
+console.log(name);
+nathan.age = 27;
+console.log(nathan);
+
+function sayAnimal() {
+  // variable animal belongs to this block scope
+  // var are function scoped, or the are function scoped or are global
+  let animal = 'cat';
+  console.log(animal);
+}
+
+sayAnimal();
